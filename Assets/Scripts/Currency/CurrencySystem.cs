74cf597a3b3d08a5fa9ab5eeb61fc4924e115206
CHAOSProd <mihocalex805@gmail.com>
2024-12-10 17:43:40 +0200

    Reapply "Fixed bucks load on re-entering"
    This reverts commit 3f775542ced123a1d6e54b8b9e6d75a0de242b75.

--- Assets/Scripts/Currency/CurrencySystem.cs
+++ Assets/Scripts/Currency/CurrencySystem.cs
@@ -9,7 +9,7 @@ public class CurrencySystem : Singleton<CurrencySystem>
     [SerializeField] private List<GameObject> _texts;
     [SerializeField] private PurchasePanel _notEnoughCoinsPanel;
 
-    private static Dictionary<CurrencyType, int> _currencyAmounts = new Dictionary<CurrencyType, int>();
+    public static Dictionary<CurrencyType, int> _currencyAmounts = new Dictionary<CurrencyType, int>();
 
     private static Dictionary<CurrencyType, TextMeshProUGUI> _currencyTexts = new Dictionary<CurrencyType, TextMeshProUGUI>();
 
@@ -22,7 +22,7 @@ public class CurrencySystem : Singleton<CurrencySystem>
     };
 
 
-    private void Awake()
+    public void Awake()
     {
         for (int i = 0; i < _texts.Count; i++)
         {
@@ -31,7 +31,7 @@ public class CurrencySystem : Singleton<CurrencySystem>
         }
     }
 
-    private void Start()
+    public void Start()
     {
         EventManager.Instance.AddListener<CurrencyChangeGameEvent>(AddCurrency);
     }
@@ -52,20 +52,20 @@ public class CurrencySystem : Singleton<CurrencySystem>
         return _currencyAmounts.ContainsKey(currencyType) && _currencyAmounts[currencyType] >= amount;
     }
 
-    private bool AddCurrency(CurrencyChangeGameEvent currencyChange)
+    public bool AddCurrency(CurrencyChangeGameEvent currencyChange)
     {
         CurrencyType currencyType = currencyChange.CurrencyType;
         int amount = currencyChange.Amount;
 
-        if (_currencyAmounts.ContainsKey(currencyType))
+        if (amount < 0 && _currencyAmounts.ContainsKey(currencyType))
         {
-            if(amount < 0 && !HasEnoughCurrency(currencyType, -amount))
+            if(!HasEnoughCurrency(currencyType, -amount))
             {
                 _notEnoughCoinsPanel.ShowNotEnoughCoinsPanel(-amount);
                 return false;
             }
             _currencyAmounts[currencyType] += amount;
-            Attributes.SetInt(currencyType.ToString(), amount);
+            Attributes.SetInt(currencyType.ToString(), _currencyAmounts[currencyType]);
             _currencyTexts[currencyType].text = _currencyAmounts[currencyType].ToString("#,#", new CultureInfo("en-US"));
             return true;
         }
