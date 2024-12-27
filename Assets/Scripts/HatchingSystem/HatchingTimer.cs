fa50db1a81ab0f1259852d56a8ad3958f4d53a14
KTZgino <160346433+KTZ-Gino@users.noreply.github.com>
2024-12-27 22:14:25 +0100

    Small fixes
    Fixed trike selected layering and removed the line that was causing the error

--- Assets/Scripts/HatchingSystem/HatchingTimer.cs
+++ Assets/Scripts/HatchingSystem/HatchingTimer.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Android.Gradle.Manifest;
 using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.UIElements;
