af86e2500a0421151f4c14e33f680077e14bdc82
CheeseRG <148676771+CheeseRG@users.noreply.github.com>
2025-01-08 17:56:24 +0100

    Update TimerBar.cs

--- Assets/Scripts/UI/TimerBar.cs
+++ Assets/Scripts/UI/TimerBar.cs
@@ -1,20 +1,30 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class TimerBar : MonoBehaviour
 {
     [SerializeField] private Transform _barForeground;
+    [SerializeField] private float remainingTimeInSeconds;
+    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private Button resetButton;
 
     private float _startX;
     private float _deltaX;
     private float _deltaScaleX;
+
     private void Awake()
     {
         Sprite sprite = _barForeground.GetComponent<SpriteRenderer>().sprite;
         _deltaScaleX = _barForeground.localScale.x;
-        _startX = _barForeground.localPosition.x - sprite.bounds.size.x * _barForeground.localScale.x * .5f;
+        _startX = _barForeground.localPosition.x - sprite.bounds.size.x * _barForeground.localScale.x * 0.5f;
         _deltaX = _barForeground.localPosition.x - _startX;
+
+        if (resetButton != null)
+        {
+            resetButton.onClick.AddListener(ResetTimeAndReduceScore);
+        }
     }
 
     public void SetProgress(float value)
@@ -30,45 +40,68 @@ public class TimerBar : MonoBehaviour
         _barForeground.localPosition = position;
     }
 
-    private IEnumerator FillOverTimeEnumerator(float seconds, VoidCallback onFinished)
+    private IEnumerator FillOverTimeEnumerator(float seconds, System.Action onFinished)
     {
         float elapsed = 0;
+        remainingTimeInSeconds = seconds;
+
         while (elapsed < seconds)
         {
             elapsed += Time.deltaTime;
+            remainingTimeInSeconds = Mathf.Max(seconds - elapsed, 0);
             SetProgress(elapsed / seconds);
             yield return new WaitForEndOfFrame();
         }
-        onFinished.Invoke();
+
+        remainingTimeInSeconds = 0;
+        onFinished?.Invoke();
     }
 
-    private IEnumerator FillOverIntervalEnumerator(float seconds, float interval, VoidCallback onInterval, VoidCallback onFinished, float startTime)
+    private IEnumerator FillOverIntervalEnumerator(float seconds, float interval, System.Action onInterval, System.Action onFinished, float startTime)
     {
         float elapsed = startTime;
         int amount = 1;
+        remainingTimeInSeconds = seconds - startTime;
+
         while (elapsed < seconds)
         {
             elapsed += Time.deltaTime;
+            remainingTimeInSeconds = Mathf.Max(seconds - elapsed, 0);
             SetProgress(elapsed / seconds);
 
-            if(elapsed >= interval * amount)
+            if (elapsed >= interval * amount)
             {
-                onInterval.Invoke();
+                onInterval?.Invoke();
                 amount++;
             }
 
             yield return new WaitForEndOfFrame();
         }
-        onFinished.Invoke();
+
+        remainingTimeInSeconds = 0;
+        onFinished?.Invoke();
     }
 
-    public void FillOverTime(float seconds, VoidCallback onFinished)
+    public void FillOverTime(float seconds, System.Action onFinished)
     {
         StartCoroutine(FillOverTimeEnumerator(seconds, onFinished));
     }
 
-    public void FillOverInterval(float seconds, float interval, VoidCallback onInterval, VoidCallback onFinished, float startTime = 0f)
+    public void FillOverInterval(float seconds, float interval, System.Action onInterval, System.Action onFinished, float startTime = 0f)
     {
-        StartCoroutine(FillOverIntervalEnumerator(seconds, interval, onInterval, onFinished,startTime));
+        StartCoroutine(FillOverIntervalEnumerator(seconds, interval, onInterval, onFinished, startTime));
+    }
+
+    public void ResetTimeAndReduceScore()
+    {
+        remainingTimeInSeconds = 0;
+        SetProgress(0);
+
+        if (scoreText != null)
+        {
+            int currentScore = int.Parse(scoreText.text);
+            currentScore = Mathf.Max(0, currentScore - 1000);
+            scoreText.text = currentScore.ToString();
+        }
     }
 }
