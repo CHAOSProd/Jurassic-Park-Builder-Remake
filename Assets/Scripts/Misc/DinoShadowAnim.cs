9c5059ac6fa11886c2bcfb7cb020ae19929b5955
KTZgino <160346433+KTZ-Gino@users.noreply.github.com>
2025-01-15 10:17:55 +0100

    Fixes / improvement
    Fixed the crash/problem happening with buildings and their max money amount, improved the shadow script in order to be applied up to 2 dino in the same enclosure, made dilo sleeping animation working

--- Assets/Scripts/Misc/DinoShadowAnim.cs
+++ Assets/Scripts/Misc/DinoShadowAnim.cs
@@ -2,17 +2,27 @@ using UnityEngine;
 
 public class DinoShadowAnim : MonoBehaviour
 {
-    [SerializeField] private Transform targetBone;
+    [SerializeField] private Transform targetBone1;
+     [SerializeField] private Transform targetBone2;
+    [SerializeField] private GameObject shadowObject1;
+    [SerializeField] private GameObject shadowObject2;
     [SerializeField] private Vector3 offset = new Vector3(0, 0.1f, 0);
     [SerializeField] private Vector3 shadowScale = new Vector3(1f, 1f, 1f);
 
     private void LateUpdate()
     {
-        if (targetBone != null)
+        if (targetBone1 != null && shadowObject1 != null)
         {
-            transform.position = targetBone.position + offset;
-            transform.rotation = Quaternion.identity;
-            transform.localScale = shadowScale;
+            shadowObject1.transform.position = targetBone1.position + offset;
+            shadowObject1.transform.rotation = Quaternion.identity;
+            shadowObject1.transform.localScale = shadowScale;
+        }
+
+        if (targetBone2 != null && shadowObject2 != null)
+        {
+            shadowObject2.transform.position = targetBone2.position + offset;
+            shadowObject2.transform.rotation = Quaternion.identity;
+            shadowObject2.transform.localScale = shadowScale;
         }
     }
 }
