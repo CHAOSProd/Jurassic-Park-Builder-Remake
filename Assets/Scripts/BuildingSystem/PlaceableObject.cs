8ba11a11b59971bb056f335fb4d40260a0e9fd4e
Pokeexplorer123 <vihanshah07@gmail.com>
2025-02-27 10:58:22 +0530

    removed polygon collider after construction

--- Assets/Scripts/BuildingSystem/PlaceableObject.cs
+++ Assets/Scripts/BuildingSystem/PlaceableObject.cs
@@ -462,6 +462,11 @@ public class PlaceableObject : MonoBehaviour
                 data.ConstructionFinished = true;
                 data.ConstructionReady = false;
                 data.Progress = null;
+                PolygonCollider2D polyCollider = GetComponent<PolygonCollider2D>();
+                if (polyCollider != null)
+                {
+                    Destroy(polyCollider);
+                }
             }
             else
             {
@@ -510,10 +515,9 @@ public class PlaceableObject : MonoBehaviour
         if (sortingGroup != null)
         {
             // Invert Y position for sorting (lower Y should be in front)
-            sortingGroup.sortingOrder = Mathf.RoundToInt(-transform.position.y * 10000);
+            sortingGroup.sortingOrder = Mathf.RoundToInt(-transform.position.y * 1000);
         }
     }
-
     #endregion
 }
 
