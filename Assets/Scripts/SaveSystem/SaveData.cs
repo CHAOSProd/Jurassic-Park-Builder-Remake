201a3726b833102dc022767098b8f54e3c955bdb
KTZgino <160346433+KTZ-Gino@users.noreply.github.com>
2025-03-08 18:57:53 +0100

    Fix
    I hope

--- Assets/Scripts/SaveSystem/SaveData.cs
+++ Assets/Scripts/SaveSystem/SaveData.cs
@@ -10,6 +10,8 @@ public class SaveData
     public List<HatchingData> HatchingData { get; set; } = new List<HatchingData>();
     public List<TreeData> TreeData { get; set; } = new List<TreeData>();
     public List<DebrisData> DebrisData { get; set; } = new List<DebrisData>();
+    public List<AmberData> AmberData { get; set; } = new List<AmberData>();
+    public List<RoadData> RoadData { get; set; } = new List<RoadData>();
     public List<AnimalShopData> AnimalShopData { get; set; } = new List<AnimalShopData>();
     public List<MoneyObjectData> MoneyData { get; set; } = new List<MoneyObjectData>();
     public Dictionary<string, object> Attributes { get; set; }
